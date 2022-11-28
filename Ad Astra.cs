ecf77a4abbd6eb054634d9f24d703df50a7fe619
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-11-28 22:31:10 +0200

    Add files via upload

---
+++ Ad Astra.cs
@@ -0,0 +1,33 @@
+﻿
+namespace _2._Ad_Astra
+{
+    using System;
+    using System.Text;
+    using System.Text.RegularExpressions;
+    using System.Collections.Generic;
+    using System.Numerics;
+    using System.Linq;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string pattern = @"((\|)?(\#)?)(?<name>[A-Za-z\ ]+)\1(?<expirationDate>(?<day>\d{2})\/(?<month>\d{2})\/(?<year>\d{2}))\1(?<calories>\d{1,5})\1";
+            Regex regex = new Regex(pattern);
+            MatchCollection matchCollection = regex.Matches(text);
+            int sumOfCalories = 0;
+            foreach (Match food in matchCollection)
+            {
+                sumOfCalories += int.Parse(food.Groups["calories"].Value);
+            }
+
+            Console.WriteLine($"You have food to last you for: {sumOfCalories/2000} days!");
+
+            foreach (Match food in matchCollection)
+            {
+                Console.WriteLine($"Item: {food.Groups["name"]}, Best before: {food.Groups["expirationDate"]}, Nutrition: {food.Groups["calories"]}");
+            }
+        }
+    }
+}
